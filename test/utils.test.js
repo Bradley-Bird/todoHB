f6d17ecf22ee0f37d782da0ff464790db57426f3
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-16 12:33:00 -0800

    test passing with no class also

--- test/utils.test.js
+++ test/utils.test.js
@@ -14,3 +14,17 @@ test('should render a li with class of complete', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected);
 });
+test('should render a li with no class', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<ul><li>thing</li></ul>`;
+
+    //Act
+    // Call the function you're testing and set the result to a const
+    const actual = renderTodo({ todo: 'thing', complete: false });
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
+
